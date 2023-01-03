0f269e31e07a54098983a1013d2bbad246aed485
al-imam <alimam01828@gmail.com>
2023-01-04 02:46:01 +0600

    make name component structure jsx

---
+++ src/components/Name/Name.tsx
@@ -0,0 +1,25 @@
+import { IoIosArrowDown } from "react-icons/io";
+import classes from "./name.module.css";
+
+const Name = () => (
+  <div>
+    <figure>
+      <img src="/assets/my-avatar.png" alt="Al Imam" />
+    </figure>
+
+    <div>
+      <h1 className="name" title="Al Imam">
+        Al Imam
+      </h1>
+
+      <p>Web developer</p>
+    </div>
+
+    <button>
+      <span>Show Contacts</span>
+      <IoIosArrowDown />
+    </button>
+  </div>
+);
+
+export default Name;
